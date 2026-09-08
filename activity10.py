e77415373a001c7bed35541afc433fa2dc6cdb4d
nobbie702-jpg <nobbie702@gmail.com>
2026-09-08 10:58:47 +0800

    Add files via upload

--- activity10.py
+++ activity10.py
@@ -5,3 +5,8 @@ username = 'marbet'
 password = 'marbet123' 
 
 print ( x >= y or username == 'marbet' and password == 'marbet123')
+
+username = 'eldon'
+password = 'eldon123'
+
+print(not(x >= y or username == 'eldon' and password == 'eldon12'))
