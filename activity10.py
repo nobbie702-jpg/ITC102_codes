3b9bc494bfc34596db7c410f3782223f78b80849
nobbie702-jpg <nobbie702@gmail.com>
2026-09-08 10:42:19 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,7 @@
+x = 10
+y = 5
+
+username = 'marbet'
+password = 'marbet123' 
+
+print ( x >= y or username == 'marbet' and password == 'marbet123')
---
+++ activity9.py
@@ -0,0 +1,18 @@
+x = 5
+y = 5
+
+user1 = "sample"
+user2 = "sample"
+#1
+print ( x > y )
+#2
+print ( x < y )
+#3
+print ( x >= y )
+#4
+print ( x <= y )
+#5
+print ( x == y )
+print ( user1 == user2 )
+#6
+print (user1 != user2 )
